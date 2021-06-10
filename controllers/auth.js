0f23035706761410b5bb31dce33fe64c5cfedfaa
leith15 <leith.mhf@gmail.com>
2021-06-10 21:24:13 +0200

    update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,14 @@
-exports.signup = (res, req) => {
-  res.send("test");
-};
+const User = require("../Models/User");
+
+
+exports.signup = (req, res) => {
+  const { name, email, password } = req.body;
+
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    }
+    let newUser=new User({name,email,password})
+    newUser.save((err,successs))
