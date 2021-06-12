427c9540d36e8b853bb926c72327eba03379f33c
leith15 <leith.mhf@gmail.com>
2021-06-13 01:24:50 +0200

    sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 const User = require("../Models/User");
 
-
 exports.signup = (req, res) => {
   const { name, email, password } = req.body;
 
@@ -10,5 +9,26 @@ exports.signup = (req, res) => {
         error: "Email is taken",
       });
     }
-    let newUser=new User({name,email,password})
-    newUser.save((err,successs))
+
+    const token = jwt.sign(
+      { name, email, password },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      { expiresIn: "10m" }
+    );
+
+    const emailData = {
+      from: process.env.EMAIL_FROM, // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+      to: email, // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE THE USER EMAIL (VALID EMAIL ADDRESS) WHO IS TRYING TO SIGNUP
+      subject: "ACCOUNT ACTIVATION LINK",
+      html: `
+                <h1>Please use the following link to activate your account</h1>
+                <p>http://localhost:3000/auth/activate/${token}</p>
+                <hr />
+                <p>This email may contain sensitive information</p>
+                <p>http://localhost:3000</p>
+            `,
+    };
+
+    sendEmailWithNodemailer(req, res, emailData);
+  });
+};
