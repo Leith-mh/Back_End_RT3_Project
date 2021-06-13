e3175a17155ea5f548800196da185829c0da50a9
leith15 <leith.mhf@gmail.com>
2021-06-13 10:50:43 +0200

    reset controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -149,3 +149,47 @@ exports.accountActivation = (req, res) => {
     });
   }
 };
+
+exports.resetPassword = (req, res) => {
+  const { resetPasswordLink, newPassword } = req.body;
+
+  if (resetPasswordLink) {
+    jwt.verify(
+      resetPasswordLink,
+      process.env.JWT_RESET_PASSWORD,
+      function (err, decoded) {
+        if (err) {
+          return res.status(400).json({
+            error: "Expired link. Try again",
+          });
+        }
+
+        User.findOne({ resetPasswordLink }, (err, user) => {
+          if (err || !user) {
+            return res.status(400).json({
+              error: "Something went wrong. Try later",
+            });
+          }
+
+          const updatedFields = {
+            password: newPassword,
+            resetPasswordLink: "",
+          };
+
+          user = _.extend(user, updatedFields);
+
+          user.save((err, result) => {
+            /*if (err) {
+              return res.status(400).json({
+                error: "Error resetting user password",
+              });
+            }*/
+            res.json({
+              message: `Great! Now you can login with your new password`,
+            });
+          });
+        });
+      }
+    );
+  }
+};
